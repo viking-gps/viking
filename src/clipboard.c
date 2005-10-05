4df1abf80bb3e9aea606dcef8138d36194f43fa4
Evan Battaglia <gtoevan@gmx.net>
2005-10-05 23:32:35 +0000

    Alex's �bercool clipboard patch

--- src/clipboard.c
+++ src/clipboard.c
@@ -19,89 +19,214 @@
  *
  */
 
+#include <stdio.h>
+#include <string.h>
 #include "viking.h"
-
-/* static for a very good reason -- share between windows */
 
 #define DATA_NONE 0
 #define DATA_LAYER 1
 #define DATA_SUBLAYER 2
 
-guint8 type = DATA_NONE;
-gint subtype;
-guint16 layer_type;
-gpointer clipboard;
+typedef struct {
+  gpointer clipboard;
+  guint8 type;
+  gint subtype;
+  guint16 layer_type;
+} vik_clipboard_t;
 
+static GtkTargetEntry target_table[] = {
+  { "application/viking", 0, 0 },
+  { "STRING", 0, 1 },
+};
+
+/**************************************
+ ** paste from clipboard functions   **
+ **************************************/
+
+static void clip_get ( GtkClipboard *c, GtkSelectionData *selection_data, guint info, gpointer p ) 
+{
+  vik_clipboard_t *vc = p;
+  g_print("clip_get\n");
+  if (info==0) {
+    gtk_selection_data_set ( selection_data, selection_data->target, 8, (void *)vc, sizeof(*vc) );
+  }
+  if (info==1) {
+    if (vc->type == DATA_LAYER) {
+      gtk_selection_data_set_text ( selection_data, VIK_LAYER(vc->clipboard)->name, -1 );
+    } 
+  }
+}
+
+static void clip_clear ( GtkClipboard *c, gpointer p )
+{
+  vik_clipboard_t *vc = p;
+  g_print("clip_clear\n");
+
+  if ( vc->clipboard && vc->type == DATA_LAYER )
+    g_object_unref ( G_OBJECT(vc->clipboard) );
+  else if ( vc->clipboard && vc->type == DATA_SUBLAYER )
+    if ( vik_layer_get_interface(vc->layer_type)->free_copied_item )
+      vik_layer_get_interface(vc->layer_type)->free_copied_item(vc->subtype,vc->clipboard);
+  vc->clipboard = NULL;
+  vc->type = DATA_NONE;
+}
+
+
+/******************************************************
+ ** functions which receive from the clipboard owner **
+ ******************************************************/
+
+/* our own data type */
+static void clip_receive_viking ( GtkClipboard *c, GtkSelectionData *sd, gpointer p ) 
+{
+  VikLayersPanel *vlp = p;
+  vik_clipboard_t *vc;
+  if (sd->length == -1) {
+    g_print("receive failed\n");
+    return;
+  } 
+  g_print("clip receive: target = %s, type = %s\n", gdk_atom_name(sd->target), gdk_atom_name(sd->type));
+  g_assert(!strcmp(gdk_atom_name(sd->target), target_table[0].target));
+  g_assert(sd->length == sizeof(*vc));
+
+  vc = (vik_clipboard_t *)sd->data;
+
+  if ( vc->clipboard && vc->type == DATA_LAYER )
+  {
+    /* oooh, _private_ ... so sue me, there's no other way to do this. */
+    if ( G_OBJECT(vc->clipboard)->ref_count == 1 )
+    { /* optimization -- if layer has been deleted, don't copy the layer. */
+      g_object_ref ( G_OBJECT(vc->clipboard) );
+      vik_layers_panel_add_layer ( vlp, VIK_LAYER(vc->clipboard) );
+    }
+    else
+    {
+      VikLayer *new_layer = vik_layer_copy ( VIK_LAYER(vc->clipboard), vik_layers_panel_get_viewport(vlp) );
+      if ( new_layer )
+      {
+        vik_layers_panel_add_layer ( vlp, new_layer );
+      }
+    }
+  }
+  else if ( vc->clipboard && vc->type == DATA_SUBLAYER )
+  {
+    VikLayer *sel = vik_layers_panel_get_selected ( vlp );
+    if ( sel && sel->type == vc->layer_type)
+    {
+      if ( vik_layer_get_interface(vc->layer_type)->paste_item )
+        vik_layer_get_interface(vc->layer_type)->paste_item ( sel, vc->subtype, vc->clipboard );
+    }
+    else
+      a_dialog_error_msg_extra ( VIK_GTK_WINDOW_FROM_WIDGET(GTK_WIDGET(vlp)), "The clipboard contains sublayer data for a %s layers. You must select a layer of this type to paste the clipboard data.", vik_layer_get_interface(vc->layer_type)->name );
+  }
+}
+
+
+/*
+ * - search for N dd.dddddd W dd.dddddd, N dd° dd.dddd W dd° dd.ddddd and so forth
+ */
+static void clip_receive_text (GtkClipboard *c, const gchar *text, gpointer p)
+{
+  VikLayersPanel *vlp = p;
+  g_print("Pasted text is ""%s""\n", text);
+}
+
+/*
+ * - copying from Mozilla seems to give html in UTF-16.
+ * - search for <span id="LatLon">...</span> like on geocaching.com and make a new waypoint
+ * - search for N dd.dddddd W dd.dddddd, N dd° dd.dddd W dd° dd.ddddd and so forth
+ */
+static void clip_receive_html ( GtkClipboard *c, GtkSelectionData *sd, gpointer p ) 
+{
+  VikLayersPanel *vlp = p;
+  gint r, w;
+  GError *err = NULL;
+  gchar *s;
+
+  if (sd->length == -1) {
+    return;
+  } 
+
+  if (!(s =  g_convert ( sd->data, sd->length, "UTF-8", "UTF-16", &r, &w, &err))) {
+    return;
+  }
+  g_print("html is %d bytes long: %s\n", sd->length, s);
+  g_free(s);
+}
+
+/*
+ * deal with various data types a clipboard may hold 
+ */
+void clip_receive_targets ( GtkClipboard *c, GdkAtom *a, gint n, gpointer p )
+{
+  VikLayersPanel *vlp = p;
+  gint i;
+
+  g_print("got targets\n");
+  for (i=0; i<n; i++) {
+    g_print("  ""%s""\n", gdk_atom_name(a[i]));
+    if (!strcmp(gdk_atom_name(a[i]), "text/html")) {
+      gtk_clipboard_request_contents ( c, gdk_atom_intern("text/html", TRUE), clip_receive_html, vlp );
+      break;
+    }
+    if (a[i] == GDK_TARGET_STRING) {
+      gtk_clipboard_request_text ( c, clip_receive_text, vlp );
+      break;
+    }
+    if (!strcmp(gdk_atom_name(a[i]), "application/viking")) {
+      gtk_clipboard_request_contents ( c, gdk_atom_intern("application/viking", TRUE), clip_receive_viking, vlp );
+      break;
+    }
+  }
+}
+
+/*********************************************************************************
+ ** public functions                                                            **
+ *********************************************************************************/
+
+/* 
+ * make a copy of selected object and associate ourselves with the clipboard
+ */
 void a_clipboard_copy ( VikLayersPanel *vlp )
 {
   VikLayer *sel = vik_layers_panel_get_selected ( vlp );
   GtkTreeIter iter;
+  vik_clipboard_t *vc = g_malloc(sizeof(*vc));
+  GtkClipboard *c = gtk_clipboard_get ( GDK_SELECTION_CLIPBOARD );
+
   if ( ! sel )
     return;
 
   vik_treeview_get_selected_iter ( sel->vt, &iter );
 
-  if ( clipboard )
-    a_clipboard_uninit();
-
   if ( vik_treeview_item_get_type ( sel->vt, &iter ) == VIK_TREEVIEW_TYPE_SUBLAYER )
   {
-    layer_type = sel->type;
-    if ( vik_layer_get_interface(layer_type)->copy_item && (clipboard = vik_layer_get_interface(layer_type)->
-        copy_item(sel,subtype=vik_treeview_item_get_data(sel->vt,&iter),vik_treeview_item_get_pointer(sel->vt,&iter)) ))
-      type = DATA_SUBLAYER;
+    vc->layer_type = sel->type;
+    if ( vik_layer_get_interface(vc->layer_type)->copy_item && (vc->clipboard = vik_layer_get_interface(vc->layer_type)->
+        copy_item(sel,vc->subtype=vik_treeview_item_get_data(sel->vt,&iter),vik_treeview_item_get_pointer(sel->vt,&iter)) ))
+      vc->type = DATA_SUBLAYER;
   }
   else
   {
-    clipboard = sel;
+    vc->clipboard = sel;
     g_object_ref ( G_OBJECT(sel) );
-    type = DATA_LAYER;
+    vc->type = DATA_LAYER;
   }
+  gtk_clipboard_set_with_data ( c, target_table, G_N_ELEMENTS(target_table), clip_get, clip_clear, vc );
 }
 
+/*
+ * to deal with multiple data types, we first request the type of data on the clipboard,
+ * and handle them in the callback
+ */
 gboolean a_clipboard_paste ( VikLayersPanel *vlp )
 {
-  if ( clipboard && type == DATA_LAYER )
-  {
-    /* oooh, _private_ ... so sue me, there's no other way to do this. */
-    if ( G_OBJECT(clipboard)->ref_count == 1 )
-    { /* optimization -- if layer has been deleted, don't copy the layer. */
-      g_object_ref ( G_OBJECT(clipboard) );
-      vik_layers_panel_add_layer ( vlp, VIK_LAYER(clipboard) );
-      return TRUE;
-    }
-    else
-    {
-      VikLayer *new_layer = vik_layer_copy ( VIK_LAYER(clipboard), vik_layers_panel_get_viewport(vlp) );
-      if ( new_layer )
-      {
-        vik_layers_panel_add_layer ( vlp, new_layer );
-        return TRUE;
-      }
-    }
-  }
-  else if ( clipboard && type == DATA_SUBLAYER )
-  {
-    VikLayer *sel = vik_layers_panel_get_selected ( vlp );
-    if ( sel && sel->type == layer_type)
-    {
-      if ( vik_layer_get_interface(layer_type)->paste_item )
-        return vik_layer_get_interface(layer_type)->paste_item ( sel, subtype, clipboard );
-    }
-    else
-      a_dialog_error_msg_extra ( VIK_GTK_WINDOW_FROM_WIDGET(GTK_WIDGET(vlp)), "The clipboard contains sublayer data for a %s layers. You must select a layer of this type to paste the clipboard data.", vik_layer_get_interface(layer_type)->name );
-    return FALSE;
-  }
-  return FALSE;
+  GtkClipboard *c = gtk_clipboard_get ( GDK_SELECTION_CLIPBOARD );
+  gtk_clipboard_request_targets ( c, clip_receive_targets, vlp );
+  return TRUE;
 }
 
 void a_clipboard_uninit ()
 {
-  if ( clipboard && type == DATA_LAYER )
-    g_object_unref ( G_OBJECT(clipboard) );
-  else if ( clipboard && type == DATA_SUBLAYER )
-    if ( vik_layer_get_interface(layer_type)->free_copied_item )
-      vik_layer_get_interface(layer_type)->free_copied_item(subtype,clipboard);
-  clipboard = NULL;
-  type = DATA_NONE;
+  /* not needed any longer */
 }
