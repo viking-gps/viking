d700ee388c42329c843e98bcdbc6ea4c4857458f
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2007-06-18 20:52:28 +0000

    Copying libcurl.m4
    This m4 module is necessary, at least to detect that libcurl is not available.
    Sort of chicken/egg problem. Looking at gettextize, it seems that the
    correct solution is to copy the m4 script of optional modules in a m4 directory
    ensuring they will be found everywhere.
    
    Signed-off-by: Guilhem Bonnefille <guilhem.bonnefille@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 # It must be used the first time the project is downloaded from the CVS.
 
 #libtoolize || exit 1
-aclocal || exit 1
+aclocal -I m4 || exit 1
 autoheader || exit 1
 automake --add-missing || exit 1
 autoconf || exit 1
