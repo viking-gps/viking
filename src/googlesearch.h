add30bb9885ecba7e35eedae994d5f14faf5e237
Bernd Zeimetz <bernd@bzed.de>
2007-10-22 17:53:20 +0000

    Fixing another implicit declaration.
    Signed-off-by: Bernd Zeimetz <bernd@bzed.de>

--- src/googlesearch.h
+++ src/googlesearch.h
@@ -3,4 +3,5 @@
 
 extern void a_google_search(VikWindow *vw, VikLayersPanel *vlp, VikViewport *vvp);
 gchar * a_googlesearch_get_search_string_for_this_place(VikWindow *vw);
+gchar *uri_escape(gchar *str);
 #endif
