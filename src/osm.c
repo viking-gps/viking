aa7b6e76dfffeb0085e2ffa59eca245e1637cf17
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2008-08-30 20:20:23 +0000

    Fix typo

--- src/osm.c
+++ src/osm.c
@@ -37,7 +37,7 @@ static int osm_maplint_download ( MapCoord *src, const gchar *dest_fn );
 static int osm_mapnik_download ( MapCoord *src, const gchar *dest_fn );
 static int osm_osmarender_download ( MapCoord *src, const gchar *dest_fn );
 static int bluemarble_download ( MapCoord *src, const gchar *dest_fn );
-static int openarealmap_download ( MapCoord *src, const gchar *dest_fn );
+static int openaerialmap_download ( MapCoord *src, const gchar *dest_fn );
 
 static DownloadOptions osm_options = { NULL, 0, a_check_map_file };
 
@@ -47,15 +47,15 @@ void osm_init () {
   VikMapsLayer_MapType mapnik_type = { 13, 256, 256, VIK_VIEWPORT_DRAWMODE_MERCATOR, osm_coord_to_mapcoord, osm_mapcoord_to_center_coord, osm_mapnik_download };  VikMapsLayer_MapType maplint_type = { 14, 256, 256, VIK_VIEWPORT_DRAWMODE_MERCATOR, osm_coord_to_mapcoord, osm_mapcoord_to_center_coord, osm_maplint_download };
 
   VikMapsLayer_MapType bluemarble_type = { 15, 256, 256, VIK_VIEWPORT_DRAWMODE_MERCATOR, osm_coord_to_mapcoord, osm_mapcoord_to_center_coord, bluemarble_download };
-  
-  VikMapsLayer_MapType openarealmap_type = { 20, 256, 256, VIK_VIEWPORT_DRAWMODE_MERCATOR, osm_coord_to_mapcoord, osm_mapcoord_to_center_coord, openarealmap_download };
+
+  VikMapsLayer_MapType openaerialmap_type = { 20, 256, 256, VIK_VIEWPORT_DRAWMODE_MERCATOR, osm_coord_to_mapcoord, osm_mapcoord_to_center_coord, openaerialmap_download };
 
   maps_layer_register_type("OpenStreetMap (Osmarender)", 12, &osmarender_type);
   maps_layer_register_type("OpenStreetMap (Mapnik)", 13, &mapnik_type);
   maps_layer_register_type("OpenStreetMap (Maplint)", 14, &maplint_type);
 
   maps_layer_register_type("BlueMarble", 15, &bluemarble_type);
-  maps_layer_register_type("OpenAerialMap", 20, &openarealmap_type);
+  maps_layer_register_type("OpenAerialMap", 20, &openaerialmap_type);
 }
 
 /* 1 << (x) is like a 2**(x) */
@@ -142,7 +142,7 @@ static int bluemarble_download ( MapCoord *src, const gchar *dest_fn )
 
 }
    
-static int openarealmap_download ( MapCoord *src, const gchar *dest_fn )
+static int openaerialmap_download ( MapCoord *src, const gchar *dest_fn )
 {
    int res = -1;
    gchar *uri = g_strdup_printf ( "/tiles/1.0.0/openaerialmap-900913/%d/%d/%d.jpg", 17-src->scale, src->x, src->y );
