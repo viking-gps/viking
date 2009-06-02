ffb5a285c1d00b5da0300e46c5615214fe3f41d6
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2009-06-02 22:15:29 +0200

    Upgrade OSM-traces support to 0.6 API

--- src/osm-traces.c
+++ src/osm-traces.c
@@ -162,7 +162,7 @@ void osm_traces_upload_file(const char *user,
   struct curl_httppost *last=NULL;
   gchar *public_string;
 
-  char *base_url = "http://www.openstreetmap.org/api/0.5/gpx/create";
+  char *base_url = "http://www.openstreetmap.org/api/0.6/gpx/create";
 
   gchar *user_pass = get_login();
 
