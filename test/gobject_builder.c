8297593f99c8aeba269ec17469584fab0d4b248d
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2009-07-19 16:11:59 +0200

    Value of property is the content of the property XML element
    This is like GtkBuilder configuration file.

--- test/gobject_builder.c
+++ test/gobject_builder.c
@@ -10,6 +10,7 @@
 
 static gchar *class_name = NULL;
 GType gtype = 0;
+gchar *property_name = NULL;
 GParameter *parameters = NULL;
 gint nb_parameters = 0;
 
@@ -29,32 +30,17 @@ _start_element (GMarkupParseContext *context,
 		gtype = g_type_from_name (class_name);
 		g_debug ("%s -> %d", class_name, gtype);
 	}
-	if (strcmp(element_name, "property") == 0)
+	if (strcmp(element_name, "property") == 0 && gtype != 0)
 	{
-		if (attribute_values[0] && attribute_values[1] && gtype != 0)
+		int i=0;
+		while (attribute_names[i] != NULL)
 		{
-			g_debug("%s=%s", attribute_values[0], attribute_values[1]);
-			nb_parameters++;
-			parameters = g_realloc(parameters, sizeof(GParameter)*nb_parameters);
-			/* parameter name */
-			parameters[nb_parameters-1].name = g_strdup(attribute_values[0]);
-			/* parameter value */
-			GObjectClass *oclass;
-			oclass = g_type_class_ref (gtype);
-			g_assert (oclass != NULL);
-			GParamSpec *pspec;
-			pspec = g_object_class_find_property (G_OBJECT_CLASS (oclass),
-                                            parameters[nb_parameters-1].name);
-			if (!pspec)
+			if (strcmp (attribute_names[i], "name") == 0)
 			{
-				g_warning ("Unknown property: %s.%s",
-				g_type_name (gtype), parameters[nb_parameters-1].name);
-				/* FIXME free unused array item */
-				return;
+				g_free (property_name);
+				property_name = g_strdup (attribute_values[i]);
 			}
-			g_debug("Expected type: %s", g_type_name (pspec->value_type));
-			memset (&(parameters[nb_parameters-1].value), 0, sizeof(GValue));
-			gtk_builder_value_from_string_type(NULL, pspec->value_type, attribute_values[1], &(parameters[nb_parameters-1].value), NULL);
+			i++;
 		}
 	}
 }
@@ -71,9 +57,22 @@ _end_element (GMarkupParseContext *context,
 	if (strcmp(element_name, "object") == 0 && gtype != 0)
 	{
 		object = g_object_newv(gtype, nb_parameters, parameters);
+		/* TODO emit signal */
+		int i = 0;
+		for (i = 0 ; i < nb_parameters ; i++)
+		{
+			g_free (parameters[i].name);
+			g_value_unset (&(parameters[i].value));
+		}
 		g_free (parameters);
 		parameters = NULL;
 		nb_parameters = 0;
+		gtype = 0;
+	}
+	if (strcmp(element_name, "property") == 0)
+	{
+		g_free (property_name);
+		property_name = NULL;
 	}
 }
 
@@ -87,6 +86,42 @@ _text (GMarkupParseContext *context,
        GError             **error)
 {
 	g_debug(__FUNCTION__);
+	if (strcmp (g_markup_parse_context_get_element (context), "property") == 0)
+	{
+		GValue gvalue = {0};
+		gboolean found = FALSE;
+		if (gtype != 0 && property_name != NULL)
+		{
+			/* parameter value */
+			GObjectClass *oclass;
+			oclass = g_type_class_ref (gtype);
+			g_assert (oclass != NULL);
+			GParamSpec *pspec;
+			pspec = g_object_class_find_property (G_OBJECT_CLASS (oclass),
+                                            property_name);
+			if (!pspec)
+			{
+				g_warning ("Unknown property: %s.%s",
+				g_type_name (gtype), property_name);
+				/* FIXME free unused array item */
+				return;
+			}
+			g_debug("Expected type: %s", g_type_name (pspec->value_type));
+			gchar *value = g_strndup (text, text_len);
+			found = gtk_builder_value_from_string_type(NULL, pspec->value_type, value, &gvalue, NULL);
+			g_free (value);
+		}
+		if (G_IS_VALUE (&gvalue) && found == TRUE)
+		{
+			/* store new parameter */
+			nb_parameters++;
+			parameters = g_realloc(parameters, sizeof(GParameter)*nb_parameters);
+			/* parameter name */
+			parameters[nb_parameters-1].name = g_strdup(property_name);
+			/* parameter value */
+			parameters[nb_parameters-1].value = gvalue;
+		}
+	}
 }
 
 int parse(gchar *text)
@@ -123,12 +158,12 @@ int main(int argc, char *argv[])
 	VIK_WEBTOOL_CENTER_TYPE;
 
 	/* Do some tests */
-	parse ("<objects><object class=\"toto\"><property name=\"titi\" value=\"tutu\"/></object></objects>");
-	parse ("<objects><object class=\"GObject\"><property name=\"titi\" value=\"tutu\"/></object></objects>");
+	parse ("<objects><object class=\"toto\"><property name=\"titi\">tutu</property></object></objects>");
+	parse ("<objects><object class=\"GObject\"><property name=\"titi\">tutu</property></object></objects>");
 	parse ("<objects><object class=\"VikWebtoolCenter\">"
-	         "<property name=\"label\" value=\"Le label\"/>"
-	         "<property name=\"url\" value=\"http://url/\"/>"
-	         "<property name=\"id\" value=\"42\"/>"
+	         "<property name=\"label\">Le label</property>"
+	         "<property name=\"url\">http://url/</property>"
+	         "<property name=\"id\">42</property>"
 	       "</object></objects>");
 
 	return EXIT_SUCCESS;
