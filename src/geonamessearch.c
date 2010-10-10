a889d67198222eaf2bd310b82b958b1031b6c112
Rob Norris <rw_norris@hotmail.com>
2010-10-10 21:03:43 +0100

    Add copyright for geonamessearch.h and correct for geonamessearch.c
    git log confirms original author as Hein Ragas.
    Thus also remove incorrect Created by statement.

--- src/geonamessearch.c
+++ src/geonamessearch.c
@@ -1,7 +1,7 @@
 /*
  * viking -- GPS Data and Topo Analyzer, Explorer, and Manager
  *
- * Copyright (C) 2003-2005, Evan Battaglia <gtoevan@gmx.net>
+ * Copyright (C) 2009, Hein Ragas
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Created by Quy Tonthat <qtonthat@gmail.com>
  */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
