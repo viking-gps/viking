153af7092ee7af6fc86c5477927f1fbafd2cf64f
Rob Norris <rw_norris@hotmail.com>
2010-10-12 00:54:28 +0100

    Fix compiler warnings in preferences.c

--- src/preferences.c
+++ src/preferences.c
@@ -212,7 +212,7 @@ void a_preferences_run_setparam ( VikLayerParamData data, VikLayerParam *params 
   preferences_run_setparam (NULL, 0, data, params);
 }
 
-static VikLayerParamData preferences_run_getparam ( gpointer notused, guint16 i )
+static VikLayerParamData preferences_run_getparam ( gpointer notused, guint16 i, gboolean notused2 )
 {
   VikLayerTypedParamData *val = (VikLayerTypedParamData *) g_hash_table_lookup ( values, ((VikLayerParam *)g_ptr_array_index(params,i))->name );
   g_assert ( val != NULL );
@@ -266,7 +266,7 @@ void a_preferences_show_window(GtkWindow *parent) {
     preferences_load_from_file();
     if ( a_uibuilder_properties_factory ( _("Preferences"), parent, contiguous_params, params_count,
 				(gchar **) groups_names->pdata, groups_names->len, // groups, groups_count, // groups? what groups?!
-                                (gboolean (*) (gpointer,guint16,VikLayerParamData,gpointer)) preferences_run_setparam,
+				(gboolean (*) (gpointer,guint16,VikLayerParamData,gpointer,gboolean)) preferences_run_setparam,
 				NULL /* not used */, contiguous_params,
                                 preferences_run_getparam, NULL /* not used */ ) ) {
       a_preferences_save_to_file();
