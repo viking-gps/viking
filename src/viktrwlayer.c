27d267f4c9a1e1dc9c3289c21ba3dd3b8b88dda3
Rob Norris <rw_norris@hotmail.com>
2013-03-17 18:41:21 +0000

    SF#3608311: Fix crash caused by renaming blank sublayer names
    Don't convert blank text into null for sublayer item names.
    Ensure string comparisons have values in rename call.

--- src/viktrwlayer.c
+++ src/viktrwlayer.c
@@ -5983,8 +5983,9 @@ static const gchar* trw_layer_sublayer_rename_request ( VikTrwLayer *l, const gc
     VikWaypoint *wp = g_hash_table_lookup ( l->waypoints, sublayer );
 
     // No actual change to the name supplied
-    if (strcmp(newname, wp->name) == 0 )
-      return NULL;
+    if ( wp->name )
+      if (strcmp(newname, wp->name) == 0 )
+       return NULL;
 
     VikWaypoint *wpf = vik_trw_layer_get_waypoint ( l, newname );
 
@@ -6013,8 +6014,9 @@ static const gchar* trw_layer_sublayer_rename_request ( VikTrwLayer *l, const gc
     VikTrack *trk = g_hash_table_lookup ( l->tracks, sublayer );
 
     // No actual change to the name supplied
-    if (strcmp(newname, trk->name) == 0)
-      return NULL;
+    if ( trk->name )
+      if (strcmp(newname, trk->name) == 0)
+	return NULL;
 
     VikTrack *trkf = vik_trw_layer_get_track ( l, (gpointer) newname );
 
@@ -6050,8 +6052,9 @@ static const gchar* trw_layer_sublayer_rename_request ( VikTrwLayer *l, const gc
     VikTrack *trk = g_hash_table_lookup ( l->routes, sublayer );
 
     // No actual change to the name supplied
-    if (strcmp(newname, trk->name) == 0)
-      return NULL;
+    if ( trk->name )
+      if (strcmp(newname, trk->name) == 0)
+        return NULL;
 
     VikTrack *trkf = vik_trw_layer_get_route ( l, (gpointer) newname );
 
