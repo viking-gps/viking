5f2a108d6904cb46fd26f019b4acc3d9a0fea3ea
Rob Norris <rw_norris@hotmail.com>
2013-04-20 01:49:54 +0100

    Fix a_vik_gpx_export_wpt_sym_name internal type

--- src/globals.c
+++ src/globals.c
@@ -136,7 +136,7 @@ void a_vik_preferences_init ()
   tmp.u = VIK_GPX_EXPORT_TRK_SORT_TIME;
   a_preferences_register(&io_prefs[1], tmp, VIKING_PREFERENCES_IO_GROUP_KEY);
 
-  tmp.u = VIK_GPX_EXPORT_WPT_SYM_NAME_TITLECASE;
+  tmp.b = VIK_GPX_EXPORT_WPT_SYM_NAME_TITLECASE;
   a_preferences_register(&io_prefs[2], tmp, VIKING_PREFERENCES_IO_GROUP_KEY);
 
 #ifndef WINDOWS
@@ -229,7 +229,7 @@ vik_gpx_export_trk_sort_t a_vik_get_gpx_export_trk_sort ( )
 vik_gpx_export_wpt_sym_name_t a_vik_gpx_export_wpt_sym_name ( )
 {
   gboolean val;
-  val = a_preferences_get(VIKING_PREFERENCES_IO_NAMESPACE "gpx_export_wpt_sym_names")->u;
+  val = a_preferences_get(VIKING_PREFERENCES_IO_NAMESPACE "gpx_export_wpt_sym_names")->b;
   return val;
 }
 
