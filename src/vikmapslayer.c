0f7e91339711b8bd13cf25bc945ff786b31f0e83
Rob Norris <rw_norris@hotmail.com>
2013-05-04 13:36:25 +0100

    Use settings to allow configuration of hardcoded map display performance values.

--- src/vikmapslayer.c
+++ src/vikmapslayer.c
@@ -26,14 +26,6 @@
 #include "config.h"
 #endif
 
-// TODO: Make these configurable
-#define MAX_TILES 1000
-
-#define MAX_SHRINKFACTOR 8.0000001 /* zoom 1 viewing 8-tiles */
-#define MIN_SHRINKFACTOR 0.0312499 /* zoom 32 viewing 1-tiles */
-
-#define REAL_MIN_SHRINKFACTOR 0.0039062499 /* if shrinkfactor is between MAX and REAL_MAX, will only check for existence */
-
 #include <gtk/gtk.h>
 #include <gdk-pixbuf/gdk-pixdata.h>
 #include <glib.h>
@@ -59,6 +51,17 @@
 #include "preferences.h"
 #include "vikmapslayer.h"
 #include "icons/icons.h"
+
+#define VIK_SETTINGS_MAP_MAX_TILES "maps_max_tiles"
+static gint MAX_TILES = 1000;
+
+#define VIK_SETTINGS_MAP_MIN_SHRINKFACTOR "maps_min_shrinkfactor"
+#define VIK_SETTINGS_MAP_MAX_SHRINKFACTOR "maps_max_shrinkfactor"
+static gdouble MAX_SHRINKFACTOR = 8.0000001; /* zoom 1 viewing 8-tiles */
+static gdouble MIN_SHRINKFACTOR = 0.0312499; /* zoom 32 viewing 1-tiles */
+
+#define VIK_SETTINGS_MAP_REAL_MIN_SHRINKFACTOR "maps_real_min_shrinkfactor"
+static gdouble REAL_MIN_SHRINKFACTOR = 0.0039062499; /* if shrinkfactor is between MAX and REAL_MAX, will only check for existence */
 
 /****** MAP TYPES ******/
 
@@ -252,6 +255,20 @@ void maps_layer_init ()
   VikLayerParamData tmp;
   tmp.s = maps_layer_default_dir();
   a_preferences_register(prefs, tmp, VIKING_PREFERENCES_GROUP_KEY);
+
+  gint max_tiles = MAX_TILES;
+  if ( a_settings_get_integer ( VIK_SETTINGS_MAP_MAX_TILES, &max_tiles ) )
+    MAX_TILES = max_tiles;
+
+  gdouble gdtmp;
+  if ( a_settings_get_double ( VIK_SETTINGS_MAP_MIN_SHRINKFACTOR, &gdtmp ) )
+    MIN_SHRINKFACTOR = gdtmp;
+
+  if ( a_settings_get_double ( VIK_SETTINGS_MAP_MAX_SHRINKFACTOR, &gdtmp ) )
+    MAX_SHRINKFACTOR = gdtmp;
+
+  if ( a_settings_get_double ( VIK_SETTINGS_MAP_REAL_MIN_SHRINKFACTOR, &gdtmp ) )
+    REAL_MIN_SHRINKFACTOR = gdtmp;
 }
 
 /****************************************/
