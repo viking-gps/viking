cae93cde01500ccc491e243e6e9873cab6d7fede
Rob Norris <rw_norris@hotmail.com>
2013-09-10 00:35:43 +0100

    Add Waypoint List dialog.
    The enables viewing lots them with their basic properties (name, date, altitude + comment).
    Being in a simple (treeview) list, each column is sortable.
    
    Implementation is very similar to the Track List dialog.

---
+++ src/viktrwlayer_waypointlist.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * viking -- GPS Data and Topo Analyzer, Explorer, and Manager
+ *
+ * Copyright (C) 2013, Rob Norris <rw_norris@hotmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+#ifndef _VIKING_TRWLAYER_WAYPOINTLIST_H
+#define _VIKING_TRWLAYER_WAYPOINTLIST_H
+
+#include "vikwaypoint.h"
+#include "viktrwlayer.h"
+
+G_BEGIN_DECLS
+
+void vik_trw_layer_waypoint_list_show_dialog ( gchar *title,
+                                               VikLayer *vl,
+                                               gpointer user_data,
+                                               VikTrwlayerGetWaypointsAndLayersFunc get_waypoints_and_layers_cb,
+                                               gboolean is_aggregate );
+
+G_END_DECLS
+
+#endif
