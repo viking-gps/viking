1d1500ec2149474207869c864bdd7ebbace7bc78
Rob Norris <rw_norris@hotmail.com>
2014-03-15 01:34:37 +0000

    [WINDOWS] Ensure an icon is embedded in the executable.
    This is particularly important for display of an icon only in the task bar on Win 7 onwards.

--- win32/configure_and_make.bat
+++ win32/configure_and_make.bat
@@ -5,6 +5,7 @@
 ::  either via part of the source download or via separate generation - e.g. using wine
 ::
 set PATH=%PATH%;%SystemDrive%\Mingw\bin;%SystemDrive%\msys\1.0\bin
-cd ..\
-sh configure CFLAGS="-DWINDOWS -mwindows" LIBCURL=-lcurldll LIBS=-lzdll --disable-realtime-gps-tracking --disable-scrollkeeper
-make
+pushd ..
+sh configure CFLAGS="-DWINDOWS -mwindows" LIBCURL=-lcurldll LIBS=-lzdll --disable-realtime-gps-tracking --disable-scrollkeeper --enable-windows
+popd
+make.bat
--- win32/make.bat
+++ win32/make.bat
@@ -1,5 +1,13 @@
 :: License: CC0
 :: Standard build
 set PATH=%PATH%;%SystemDrive%\MinGW\bin;%SystemDrive%\msys\1.0\bin
-cd ..\src
+
+:: The icon needs creating first as it will get embedded in the generated executable
+echo Create Icon
+pushd installer\pixmaps
+windres.exe viking_icon.rc -o viking_icon.o
+popd
+
+:: Change to root directory and build
+cd ..
 make
