fff8bf2d31385b638ffdded74b66885758531e4a
Rob Norris <rw_norris@hotmail.com>
2014-03-16 11:07:56 +0000

    Notes on how to use git2changelog.sh
    Since I always forget the format of the parameters passed to git log

--- maintainer/git2changelog.sh
+++ maintainer/git2changelog.sh
@@ -25,6 +25,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+# e.g. for updating the NEWS file before tagging the final commit
+# such as for release 1.5.1 or 1.6, to only list the changes since previous main release (1.5):
+# ./git2changelog.sh HEAD..viking-1.5
+#
 git log --date=short "--format=%cd %an <%aE>:%n%x09* %s" "$@" \
 | awk '/^[^\t]/{if(previous==$0)next;previous=$0}{print}' \
 | sed '/^[^\t]/s/\([^ ]*\) \(.*\)/\n\1\n\2/'
