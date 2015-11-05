7cb69eeece8a9d68083419aa67e0b056dc0f4248
Rob Norris <rw_norris@hotmail.com>
2015-11-05 17:56:39 +0000

    Make OSM services the default by including these modules first.

--- src/modules.c
+++ src/modules.c
@@ -235,6 +235,11 @@ register_loadable_types(void)
  */
 void modules_init()
 {
+// OSM done first so this will be the default service for searching/routing/etc...
+#ifdef VIK_CONFIG_OPENSTREETMAP
+  osm_init();
+  osm_traces_init();
+#endif
 #ifdef VIK_CONFIG_BING
   bing_init();
 #endif
@@ -246,10 +251,6 @@ void modules_init()
 #endif
 #ifdef VIK_CONFIG_TERRASERVER
   terraserver_init();
-#endif
-#ifdef VIK_CONFIG_OPENSTREETMAP
-  osm_init();
-  osm_traces_init();
 #endif
 #ifdef VIK_CONFIG_BLUEMARBLE
   bluemarble_init();
