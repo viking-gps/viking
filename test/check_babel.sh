abcff3f7715eedf1557c2b6dcf7edc53ac474571
Rob Norris <rw_norris@hotmail.com>
2016-04-05 22:18:26 +0100

    Check that the test_babel program runs

---
+++ test/check_babel.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Run the test_babel program
+#  Don't actually care what the output is (i.e. don't care if gpsbabel is available or not)
+#  Just confirm that the program runs at all
+./test_babel 1 0 1 0 1 0
