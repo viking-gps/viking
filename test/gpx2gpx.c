74e6769aacc701dd8c6c406d5ee8f14798dff043
Rob Norris <rw_norris@hotmail.com>
2018-01-31 12:14:46 +0000

    Fix gpx2gpx.c for latest function changes

--- test/gpx2gpx.c
+++ test/gpx2gpx.c
@@ -20,8 +20,8 @@ int main(int argc, char *argv[])
   VikLayer *vl = vik_layer_create (VIK_LAYER_TRW, NULL, FALSE);
   VikTrwLayer *trw = VIK_TRW_LAYER (vl);
 
-  a_gpx_read_file(trw, stdin);
-  a_gpx_write_file(trw, stdout, NULL);
+  a_gpx_read_file(trw, stdin, NULL);
+  a_gpx_write_file(trw, stdout, NULL, NULL);
   // NB no layer_free functions directly visible anymore
   //  automatically called by layers_panel_finalize cleanup in full Viking program
 
