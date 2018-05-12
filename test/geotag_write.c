c475021b61ab4e6b416d60547cbbb8595ee968c4
Rob Norris <rw_norris@hotmail.com>
2018-05-12 12:49:47 +0100

    Fix test program geotag_write for updated function interface.
    ATM tests don't include direction image attributes.

--- test/geotag_write.c
+++ test/geotag_write.c
@@ -1,5 +1,6 @@
 // Copyright: CC0
 #include <stdio.h>
+#include <math.h>
 #include <glib.h>
 #include <glib/gstdio.h>
 #include "geotag_exif.h"
@@ -16,7 +17,9 @@ int main(int argc, char *argv[])
     struct LatLon ll = { 51.179489, -1.826217 };
     VikCoord vc;
     vik_coord_load_from_latlon ( &vc, VIK_COORD_LATLON, &ll );
-    answer = a_geotag_write_exif_gps ( argv[1], vc, 0.0, TRUE );
+    // NB sqrt(-1) is delibrate to generate a NaN value
+    //  (so no image direction EXIF tags are generated)
+    answer = a_geotag_write_exif_gps ( argv[1], vc, 0.0, sqrt(-1), WP_IMAGE_DIRECTION_REF_TRUE, TRUE );
   }
  
   return answer;
