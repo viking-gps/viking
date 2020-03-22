5ac82757b9664d774596e02e7239e3f668d712ba
Rob Norris <rw_norris@hotmail.com>
2020-03-22 15:28:49 +0000

    Remove unnecessary define

--- src/gpx.c
+++ src/gpx.c
@@ -26,8 +26,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-#define _XOPEN_SOURCE /* glibc2 needs this */
-
 #include "gpx.h"
 #include "viking.h"
 #include <expat.h>
