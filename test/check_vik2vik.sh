7f1a1cc868e05eb18d458e4ad24aee41d3c77597
Rob Norris <rw_norris@hotmail.com>
2020-12-07 22:27:41 +0000

    Enable variant testing for optional build components that affects .vik file output
    Make use of AM_TESTS_ENVIRONMENT to pass build information into test scripts.
    
    Fixes build test with realtime_gps_tracking disabled.
    
    Restore GeoClue layer into the full test file and add a version without it.

--- test/check_vik2vik.sh
+++ test/check_vik2vik.sh
@@ -6,7 +6,20 @@ if [ -z "$srcdir" ]; then
 fi
 
 outfile=./testout-$$.vik
-result=$(./vik2vik < $srcdir/Simple.vik $outfile)
+
+# ATM Tests either full, no libgps or no geoclue
+# Not going to try to cover all potential options, such as
+#  no gps && no geoclue at the same time
+
+if [ -z "$REALTIME_GPS_TRACKING" ]; then
+    testvik=$srcdir/Simple_no-realtime-gps-tracking.vik
+elif [ -z "$GEOCLUE_ENABLED" ]; then
+    testvik=$srcdir/Simple_no-geoclue.vik
+else
+    testvik=$srcdir/Simple.vik
+fi
+
+result=$(./vik2vik < $testvik $outfile)
 if [ $? != 0 ]; then
   echo "vik2vik command failure"
   exit 1
@@ -14,7 +27,7 @@ fi
 
 # Avoid maps directory as a blank input value may get saved with a user path specific default
 sed -i '/^directory=/d' $outfile
-grep -v "^directory=" $srcdir/Simple.vik | diff $outfile -
+grep -v "^directory=" $testvik | diff $outfile -
 if [ $? != 0 ]; then
   echo "vik2vik produced different result"
   exit 1
