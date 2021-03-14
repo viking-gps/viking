0ed6c2ac6f15a45bda9a461ed7c02b0e76053342
Rob Norris <rw_norris@hotmail.com>
2021-03-14 11:13:49 +0000

    GTK3: Ensure lines are drawn in at desired width
    Need to adjust into cairo coordinates for better consistency

--- src/vikviewport.c
+++ src/vikviewport.c
@@ -1566,7 +1566,7 @@ void vik_viewport_draw_line ( VikViewport *vvp, GdkGC *gc, gint x1, gint y1, gin
     cairo_set_line_width ( gc, thickness );
     if ( gcolor )
       gdk_cairo_set_source_color ( gc, gcolor );
-    ui_cr_draw_line ( gc, x1, y1, x2, y2 );
+    ui_cr_draw_line ( gc, x1-0.5, y1-0.5, x2-0.5, y2-0.5 );
     cairo_stroke ( gc );
 #else
     gdk_draw_line ( vvp->scr_buffer, gc, x1, y1, x2, y2);
