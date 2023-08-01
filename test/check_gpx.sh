ccd5d0fab1ca703ec0c78a10e657589bd2f29700
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2023-08-01 15:17:53 +0200

    fix: rename test data as expected
    fix the 6edfa9c211e7970cb88b1a1076bbd0141f9de398

--- test/check_gpx.sh
+++ test/check_gpx.sh
@@ -37,7 +37,7 @@ count=`expr $count + 1`
 # if the file that is marked as v1.0 - then it gets exported as v1.0
 # (even if v1.1 extension fields are read in)
 grep -v "^creator=" $srcdir/GH#137-v1.0.gpx > ./GH#137-v1.0-nocreator.gpx
-result=$(./gpx2gpx < $srcdir/GH#137.gpx | \
+result=$(./gpx2gpx < $srcdir/GH#137-v1.0.gpx | \
              grep -v "^creator=" | \
              diff ./GH#137-v1.0-nocreator.gpx -)
 if [ $? != 0 ]; then
