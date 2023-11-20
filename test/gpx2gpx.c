2a4212cc4a184eba7869949eb256b814d018c1f6
Rob Norris <rw_norris@hotmail.com>
2023-11-20 17:51:18 +0000

    [QA] Specifically ignoring function result here

--- test/gpx2gpx.c
+++ test/gpx2gpx.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
   VikLayer *vl = vik_layer_create (VIK_LAYER_TRW, NULL, FALSE);
   VikTrwLayer *trw = VIK_TRW_LAYER (vl);
 
-  a_gpx_read_file(trw, stdin, NULL, FALSE);
+  (void)a_gpx_read_file(trw, stdin, NULL, FALSE);
   a_gpx_write_file(trw, stdout, NULL, NULL);
 
   g_object_unref ( vl );
