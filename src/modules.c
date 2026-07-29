ec33851ed4434619d1642dff5036861d4407efb2
Matthew Hague <matthewhague@zoho.com>
2026-07-29 11:20:07 +0100

    Add makefile flags for adding/omitting maplibre
    AI: Claude Sonnet 4.6

--- src/modules.c
+++ src/modules.c
@@ -62,9 +62,9 @@
 #ifdef HAVE_LIBMAPNIK
 #include "vikmapniklayer.h"
 #endif
-//TODO:if MAPLIBRE
+#ifdef HAVE_MAPLIBRE
 #include "vikmaplibrelayer.h"
-//endif
+#endif
 
 #define VIKING_MAPS_FILE "maps.xml"
 #define VIKING_EXTTOOLS_FILE "external_tools.xml"
@@ -290,9 +290,9 @@ void modules_post_init ()
 #ifdef HAVE_LIBMAPNIK
   vik_mapnik_layer_post_init();
 #endif
-//TODO: IF MAPLIBRE
+#ifdef HAVE_MAPLIBRE
   vik_maplibre_layer_post_init();
-//endif
+#endif
 }
 
 /**
